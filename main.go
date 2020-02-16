5597b7f4abd861b957463162fc74ed89ee226a10
rcvalenteai <rcvalenteai@gmail.com>
2020-02-16 15:24:42 -0500

    lambda integration

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"time"
 )
 
 func add(a int, b int) int {
@@ -10,6 +12,25 @@ func add(a int, b int) int {
 	return c
 }
 
+type Request struct {
+	A int `json:"id"`
+	B int `json:"value"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+	Ok      bool   `json:"ok"`
+	Value   int    `json:"value"`
+}
+
+func Handler(request Request) (Response, error) {
+	fmt.Println(time.Now())
+	return Response{
+		Message: fmt.Sprintf("Adding Values %d and %d", request.A, request.B),
+		Ok:      true,
+		Value:   add(request.A, request.B)}, nil
+}
+
 func main() {
-	add(5, 3)
+	lambda.Start(Handler)
 }

17cc81c64842c470a80e359727637546ed156690
rcvalenteai <rcvalenteai@gmail.com>
2020-02-18 14:42:45 -0500

    added API gateway code

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"net/http"
+	//"net/rpc"
+	"encoding/json"
+	"os"
+	"strconv"
 	"time"
 )
+
+var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
 
 func add(a int, b int) int {
 	c := a + b
@@ -27,6 +36,35 @@ type Response struct {
 	Value   int    `json:"value"`
 }
 
+func show(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	a, err := strconv.Atoi(req.QueryStringParameters["a"])
+	b, err := strconv.Atoi(req.QueryStringParameters["b"])
+	if err != nil {
+		return serverError(err)
+	}
+	js, err := json.Marshal(getResponse(a, b))
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(js),
+	}, nil
+}
+
+func getResponse(a int, b int) Response {
+	return Response{
+		Message: fmt.Sprintf("Multiplying Values %d and %d", a, b),
+		Ok:      true,
+		Value:   multiply(a, b)}
+}
+
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	errorLogger.Println(err.Error())
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       http.StatusText(http.StatusInternalServerError),
+	}, nil
+}
+
 func Handler(request Request) (Response, error) {
 
 	fmt.Println(time.Now())
@@ -37,5 +75,5 @@ func Handler(request Request) (Response, error) {
 }
 
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(show)
 }

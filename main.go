5e1fc6860c9e0a40d229ada6c8cd34fe67143e32
rcvalenteai <rcvalenteai@gmail.com>
2020-02-23 20:55:44 -0500

    mongoDB code

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
 	//"net/rpc"
 	"encoding/json"
+	"go.mongodb.org/mongo-driver/mongo"
 	"os"
 	"strconv"
 	"time"
@@ -51,6 +54,7 @@ func show(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, er
 }
 
 func getResponse(a int, b int) Response {
+	sendMongo()
 	return Response{
 		Message: fmt.Sprintf("Multiplying Values %d and %d", a, b),
 		Ok:      true,
@@ -73,6 +77,38 @@ func Handler(request Request) (Response, error) {
 		Message: fmt.Sprintf("Multiplying Values %d and %d", request.A, request.B),
 		Ok:      true,
 		Value:   multiply(request.A, request.B)}, nil
+}
+
+type Person struct {
+	Name string
+	Age  int
+	City string
+}
+
+func sendMongo() {
+	password := os.Getenv("MONGO_PASS")
+	url := "mongodb+srv://bigdatamanagement:" + password + "@mongosandbox-qzmsu.mongodb.net/test?retryWrites=true&w=majority"
+	clientOptions := options.Client().ApplyURI(url)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("projectfour").Collection("datavisthree")
+	ruan := Person{"Ruan", 34, "Cape Town"}
+	insertResult, err := collection.InsertOne(context.TODO(), ruan)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted a Single Document: ", insertResult.InsertedID)
+
 }
 
 func main() {
